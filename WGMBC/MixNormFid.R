7609b0cdad730732bb0448465ca05d8b66a21f3e
Derek S. Young <54218160+dsy109@users.noreply.github.com>
2023-07-19 22:12:45 -0400

    Updated function

--- WGMBC/MixNormFid.R
+++ WGMBC/MixNormFid.R
@@ -2,7 +2,7 @@ library(mixtools)
 library(mixR)
 library(ggplot2)
 
-GMM.fid <- function(x,w=NULL,k,M){
+GMM.fid <- function(x,w=NULL,k,M,min.nj=2){
   n <- length(x)
   if(is.null(w)) w <- sample(1:k,n,rep=T)
   R.sig2.j <- function(x,w) sapply(1:max(w),function(i) (sum(w==i)-1)*var(x[w==i])/rchisq(1,sum(w==i)-1) ) 
@@ -25,7 +25,7 @@ GMM.fid <- function(x,w=NULL,k,M){
     r <- exp(r.num-r.den)
     if(is.na(r)) r <- 1
     accept <- rbinom(1,1,min(r,1))
-    if(accept & min(table(w.star))>1) w <- w.star
+    if(accept & min(table(w.star))>=min.nj) w <- w.star
     
     r.j <- cumsum(sapply(1:max(w),function(i) sum(w==i)))
     U <- c(0,sort(runif(n)),1)
@@ -68,11 +68,31 @@ set.seed(10)
 out.fid.stamp <- GMM.fid(Stamp,k=4,M=100000)
 set.seed(10)
 out.EM.stamp <- normalmixEM(Stamp,k=4,eps=1e-10,maxit=10000)
-sapply(1:3,function(i) apply(out.fid.stamp[[i]][-c(1:burn.in),],2,mean))
+out.fid.stamp2 <- sapply(1:3,function(i) apply(out.fid.stamp[[i]][-c(1:burn.in),],2,mean))
+out.fid.stamp2
 out.EM.stamp[2:4]
 
-
-
+Stamps <- data.frame(Stamp)
+ggplot(Stamps, aes(x=Stamp)) + geom_histogram(color="#FFFFFF") + ggtitle("Hidalgo Stamp Data") + theme(text = element_text(size = 15)) + xlim(c(0.055,0.135))
+X <- seq(0.055,0.135,length=200)
+DF <- data.frame(x=X,y1=out.fid.stamp2[1,1]*dnorm(X,out.fid.stamp2[1,2],sqrt(out.fid.stamp2[1,3])),y2=out.fid.stamp2[2,1]*dnorm(X,out.fid.stamp2[2,2],sqrt(out.fid.stamp2[2,3])),
+                 y3=out.fid.stamp2[3,1]*dnorm(X,out.fid.stamp2[3,2],sqrt(out.fid.stamp2[3,3])),y4=out.fid.stamp2[4,1]*dnorm(X,out.fid.stamp2[4,2],sqrt(out.fid.stamp2[4,3])))
+ggplot(Stamps, aes(x=Stamp)) + geom_histogram(aes(y=..density..),color="#FFFFFF") + 
+  layer(geom="area",data=DF,stat="identity",position="identity",mapping=aes(x=x,y=y1),params = list(fill = "#56B4E9", alpha = 0.5)) + 
+  layer(geom="area",data=DF,stat="identity",position="identity",mapping=aes(x=x,y=y2),params = list(fill = "#56E98B", alpha = 0.5)) + 
+  layer(geom="area",data=DF,stat="identity",position="identity",mapping=aes(x=x,y=y3),params = list(fill = "#E956B4", alpha = 0.5)) + 
+  layer(geom="area",data=DF,stat="identity",position="identity",mapping=aes(x=x,y=y4),params = list(fill = "#E98B56", alpha = 0.5)) + 
+  geom_line(data=DF,aes(x=x,y=y1+y2+y3+y4),color="black",lwd=.18,alpha=.8) + ggtitle("Fiducial Solution") +
+  theme(text = element_text(size = 15))
+DF.EM <- data.frame(x=X,y1=out.EM.stamp$lambda[1]*dnorm(X,out.EM.stamp$mu[1],out.EM.stamp$sigma[1]),y2=out.EM.stamp$lambda[2]*dnorm(X,out.EM.stamp$mu[2],out.EM.stamp$sigma[2]),
+                 y3=out.EM.stamp$lambda[3]*dnorm(X,out.EM.stamp$mu[3],out.EM.stamp$sigma[3]),y4=out.EM.stamp$lambda[4]*dnorm(X,out.EM.stamp$mu[4],out.EM.stamp$sigma[4]))
+ggplot(Stamps, aes(x=Stamp)) + geom_histogram(aes(y=..density..),color="#FFFFFF") + 
+  layer(geom="area",data=DF.EM,stat="identity",position="identity",mapping=aes(x=x,y=y1),params = list(fill = "#56B4E9", alpha = 0.5)) + 
+  layer(geom="area",data=DF.EM,stat="identity",position="identity",mapping=aes(x=x,y=y2),params = list(fill = "#56E98B", alpha = 0.5)) + 
+  layer(geom="area",data=DF.EM,stat="identity",position="identity",mapping=aes(x=x,y=y3),params = list(fill = "#E956B4", alpha = 0.5)) + 
+  layer(geom="area",data=DF.EM,stat="identity",position="identity",mapping=aes(x=x,y=y4),params = list(fill = "#E98B56", alpha = 0.5)) + 
+  geom_line(data=DF.EM,aes(x=x,y=y1+y2+y3+y4),color="black",lwd=.18,alpha=.8) + ggtitle("EM Solution") +
+  theme(text = element_text(size = 15))
 
 
 
