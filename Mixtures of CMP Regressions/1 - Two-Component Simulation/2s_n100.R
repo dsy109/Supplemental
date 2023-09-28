41081175d747bfd9446ef99e5b3691511a9304ad
Derek S. Young <54218160+dsy109@users.noreply.github.com>
2023-09-28 12:56:31 -0400

    Add manuscript R scripts

---
+++ Mixtures of CMP Regressions/1 - Two-Component Simulation/2s_n100.R
@@ -0,0 +1,111 @@
+###################################################################################
+## Monte Carlo Simulation to test the model of CMPs mixture Regression
+###################################################################################
+
+setwd("/Users/derekyoung/Documents/CMP Research/JSTP Revision/Simulations/")
+source("Dongying_functions.R")
+source("Derek_Functions.R")
+
+set.seed(1)   # seed
+
+
+#################################################################################
+## 2-component mixture of mean-parametrized CMP
+## Simulate the response variable y and covariate x according to mean-parameterized CMP mixtures
+## sample size n=50,100,200
+#################################################################################
+
+start_time <- Sys.time()
+
+## setting
+n <- 100      # sample size
+B <- 1000    # replicates
+
+# covariate x
+x <- seq(0,10,length=n)  
+X <- cbind(1,x) 
+
+k=2 # number of components
+q=ncol(X) # number of columns for covariates
+
+# beta
+beta01=0.5   ;beta11=-0.3  # com1 - upper line 
+beta02=0.6   ;beta12=0.4  # com2 - bottom line
+
+beta <- matrix(c(beta01,beta11,beta02,beta12),nrow=q,ncol=k)
+
+# dispersion
+nu1 <- 0.2 
+nu2 <- 4.0   
+
+# mixing proportion
+Pi <- 0.5     
+
+# data to save  
+y.B <- c()       # sample point
+y.E <- c()       # points generated for non-convergent samples
+MLE.cmp <- c()   # estimates
+
+i = 0
+while(i < B) {
+  i <- i+1
+  
+  print(paste("B =",i))
+  
+  # simulate the component response y based on x
+  
+  z <- rbinom(n,1,Pi)                 #component label 
+  y <- rep(0,n)
+  ind.1 <- which(z==1)
+  ind.2 <- which(z==0)
+  mu.true <- exp(X%*%beta)
+  y[ind.1] <- rcomp(length(ind.1),mu.true[ind.1,1],nu=nu1)
+  y[ind.2] <- rcomp(length(ind.2),mu.true[ind.2,2],nu=nu2)
+  
+  # regress CMP mixtures
+  tmp.test <- TRUE
+  tmp.ind <- 0
+  while(tmp.test){
+    tmp.ind <- tmp.ind + 1
+    out.cmp <- try(withTimeout(mix.mpcmp2(y,x,k=2,
+                                          start=list(beta=beta,lambda=c(Pi,1-Pi),nu=c(nu1,nu2)),
+                                          eps=1e-3,maxit=1000),timeout=20,onTimeout = "silent"),silent=TRUE)
+    tmp.test <- ifelse(class(out.cmp)=="try-error",T,F)
+    if(tmp.ind == 30){
+      tmp.test <- FALSE
+      out.cmp <- NULL
+    }
+  }
+  if(is.null(out.cmp)){
+    i <- i-1
+    y.E <- rbind(y.E, y) #response variable y (bad samples)
+  } else{
+    y.B <- rbind(y.B, y) #response variable y (good samples)
+    MLE.cmp <- rbind(MLE.cmp,
+                     c(length(out.cmp$all.ll),tail(out.cmp$all.ll,1),out.cmp$lambda,c(out.cmp$betas),out.cmp$nus,use.names=F))
+    
+  if(i%in%seq(50,1000,by=50)) save.image(file=paste("2s_n",n,".RData",sep=""))
+  }
+}
+
+end_time <- Sys.time()
+
+
+## save .RData
+save.image(file=paste("2s_n",n,".RData",sep=""))
+
+
+## save to txt files
+
+write(paste("Time is", end_time-start_time, attr(end_time-start_time,"units")), 
+      file=paste("2s_n",n,"_time",".txt",sep=""))
+
+write.table(y.B, file=paste("2s_n",n,"_samples",".txt",sep=""), sep="  ", row.names=1:B)
+write.table(y.E, file=paste("2s_n",n,"_badsamples",".txt",sep=""), sep="  ")
+
+write.table(MLE.cmp, file=paste("2s_n",n,"_MLEs",".txt",sep=""), sep="  ", row.names=1:B)
+
+
+
+
+
